132d3007c4089b696af613131a968bfdd376f467
kaur-dilpreet <dilpreet.kaur992@gmail.com>
2024-12-13 13:20:40 +0530

    Common helper functions

--- src/components/Calculator/__tests__/Calculator.test.tsx
+++ src/components/Calculator/__tests__/Calculator.test.tsx
@@ -1,39 +1,41 @@
-import { shallow } from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
 import Calculator from "../Calculator";
 
 describe('Calculator Component', () => {
+  let wrapper: ShallowWrapper;
+
+  // Helper function to simulate user input and click
+  const simulateInputAndClick = (inputValue: string): void => {
+    const inputWrapper = wrapper.find('Input').dive();
+    const input = inputWrapper.find('input');
+    input.simulate('change', { target: { value: inputValue } });
+
+    const buttonWrapper = wrapper.find('Button').dive();
+    const button = buttonWrapper.find('button');
+    button.simulate('click');
+  };
+
+  // Helper function to get the output text
+  const getOutputText = (): string => {
+    const calculatorOutputWrapper = wrapper.find('CalculatorOutput').dive();
+    return calculatorOutputWrapper.find('.calculator-output div').text();
+  };
+
+  beforeEach(() => {
+    wrapper = shallow(<Calculator />);
+  });
+
   it('should render the component correctly', () => {
-    const wrapper = shallow(<Calculator />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('sum for empty value', () => {
-    const wrapper = shallow(<Calculator />);
-
-    const inputWrapper = wrapper.find('Input').dive();
-    const input = inputWrapper.find('input');
-    input.simulate('change', { target: { value: '' } });
-
-    const buttonWrapper = wrapper.find('Button').dive();
-    const button = buttonWrapper.find('button');
-    button.simulate('click');
-
-    const calculatorOutputWrapper = wrapper.find('CalculatorOutput').dive();
-    expect(calculatorOutputWrapper.find('.calculator-output div').text()).toContain('Result: 0');
+    simulateInputAndClick('');
+    expect(getOutputText()).toContain('Result: 0');
   });
 
   it('sum for single number', () => {
-    const wrapper = shallow(<Calculator />);
-
-    const inputWrapper = wrapper.find('Input').dive();
-    const input = inputWrapper.find('input');
-    input.simulate('change', { target: { value: '2' } });
-
-    const buttonWrapper = wrapper.find('Button').dive();
-    const button = buttonWrapper.find('button');
-    button.simulate('click');
-
-    const calculatorOutputWrapper = wrapper.find('CalculatorOutput').dive();
-    expect(calculatorOutputWrapper.find('.calculator-output div').text()).toContain('Result: 2');
+    simulateInputAndClick('2');
+    expect(getOutputText()).toContain('Result: 2');
   });
 });
